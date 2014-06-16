b79782f762deec5ceeedeebb7d82b1746f522ebd
kain <me@kain-jy.com>
2014-06-17 06:28:27 +0900

    Init

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+# Note: We don't need to call run() since our application is embedded within
+# the App Engine WSGI application server.
+
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    return 'Hello World!'
+
+
+@app.route('/events')
+def events():
+    return 'This is event lists.'
+
+
+@app.route('/me')
+def me():
+    return 'This is my status.'
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """Return a custom 404 error."""
+    return 'Sorry, nothing at this URL.', 404
